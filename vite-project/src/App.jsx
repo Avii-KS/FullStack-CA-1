d1d56a550f6d7c5b4cab4a080b60d7e76d56ac81
Avii-KS <avi.k.s.4653@gmail.com>
2025-03-04 14:35:50 +0530

    Changes Updated

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import TestimonialCard from "./components/TestimonialCard.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <TestimonialCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/components/TestimonialCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TestimonialCard = () => {
+  return (
+    <div>
+      <h2>Emily Watson</h2>
+      <p>This service was amazing! Highly recommended.</p>
+      <h2>John Doe</h2>
+      <p>It was a great Experience</p>
+    </div>
+  );
+};
+
+export default TestimonialCard;
